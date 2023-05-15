459bcb736ffabb265ee4b102b88efb519de93a63
abdulhafiz001 <abdulhafizharuna2929@gmail.com>
2023-05-15 12:58:34 +0100

    changes

---
+++ HTML/footer.php
@@ -0,0 +1,131 @@
+ <!-- FOOTER START
+            ============================================= -->
+            <footer id="footer" class="wrapper clearfix">
+
+                <div class="container">
+                    <div class="row">
+                        <div class="footer-wrap row">
+                            <!-- WIDGET FOOTER 1 START
+                            ============================================= -->
+                            <div class="widget-footer col-md-3">
+                                <div class="short-desc">
+                                    <div class="logo-footer">
+                                        <img src="img/logo-footer.png" alt="" />
+                                    </div>
+                                    <p>Phasellus et nisl tellus. Etiam facilisis eu nisi scelerisque faucibus. Proin semper suscipit magna, nec imperdiet lacus semper vitae. Sed hendrerit enim non justo posuere placerat eget purus mauris.</p>
+                                    <p>Etiam facilisis eu nisi scelerisque fabus. Proin semper suscipit magna, nec pediet lacus semper.</p>
+                                </div>
+                            </div>
+                            <!-- WIDGET FOOTER 1 END -->
+
+                            <!-- WIDGET FOOTER 2 START
+                            ============================================= -->
+                            <div class="widget-footer col-md-3">
+                                <div class="recent-post">
+                                    <h4 class="title">Recent Post</h4>
+                                    <div class="footer-content">
+                                        <div class="post-item">
+                                            <a href="single-post.html">
+                                                <div class="post-thumb">
+                                                    <img src="img/content/recent-post-thumb-1.jpg" alt="" />
+                                                    <div class="overlay"></div>
+                                                </div>
+                                            </a>
+                                            <div class="post-content">
+                                                <a href="single-post.html">
+                                                    <h4>Your Blog Title Here</h4>
+                                                </a>
+                                                <p>Phasellus et nisl tellus. Etiam facilisis eu nisi scelerisque faucibus...</p>
+                                                <div class="meta">
+                                                    <span class="date">02 August, 2015</span>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="post-item">
+                                            <a href="single-post.html">
+                                                <div class="post-thumb">
+                                                    <img src="img/content/recent-post-thumb-2.jpg" alt="" />
+                                                    <div class="overlay"></div>
+                                                </div>
+                                            </a>
+                                            <div class="post-content">
+                                                <a href="single-post.html">
+                                                    <h4>Your Blog Title Here</h4>
+                                                </a>
+                                                <p>Phasellus et nisl tellus. Etiam facilisis eu nisi scelerisque faucibus...</p>
+                                                <div class="meta">
+                                                    <span class="date">02 August, 2015</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- WIDGET FOOTER 2 END -->
+
+                            <!-- WIDGET FOOTER 3 START
+                            ============================================= -->
+                            <div class="widget-footer col-md-3">
+                                <div class="contact-footer">
+                                    <h4 class="title">Contact Us</h4>
+                                    <div class="footer-content">
+                                        <div class="contact-section">
+                                            <h4>Our Office Address</h4>
+                                            <p>Boulevard des Capucines</p>
+                                            <p>356, Coffee Street</p>
+                                            <p>Paris, France</p>
+                                            <p>Telephone : + 1 555 356 876</p>
+                                            <p>Email : info@themesawesome.com</p>
+                                        </div>
+                                        <div class="contact-section">
+                                            <h4>Our Storage Address</h4>
+                                            <p>Boulevard des Capucines</p>
+                                            <p>356, Coffee Street</p>
+                                            <p>Paris, France</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- WIDGET FOOTER 3 END -->
+
+                            <!-- WIDGET FOOTER 4 START
+                            ============================================= -->
+                            <div class="widget-footer col-md-3">
+                                <div class="buy-now">
+                                    <h4 class="title">Buy Now !</h4>
+                                    <div class="footer-content">
+                                        <p>Phasellus et nisl tellus. Etiam facilisis eu nisi scelerisque faucibus. Proin semper suscipit magna, nec imperdiet lacus semper vitae. Sed hendrerit enim non justo posuere placerat eget purus mauris.</p>
+
+                                        <a href="#" class="button-normal white">Buy Now</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- WIDGET FOOTER 4 END -->
+                        </div>
+                    </div>
+                </div>
+
+                <!-- COPYRIGHT START
+                ============================================= -->
+                <div class="copyright text-center">
+                    <div class="container">
+                        <div class="social-icon">
+                            <ul>
+                                <li><a href="http://facebook.com"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="http://twitter.com"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="http://tumblr.com"><i class="fa fa-tumblr"></i></a></li>
+                                <li><a href="http://vimeo.com"><i class="fa fa-vimeo"></i></a></li>
+                                <li><a href="http://linkedin.com"><i class="fa fa-linkedin"></i></a></li>
+                                <li><a href="http://dribbble.com"><i class="fa fa-dribbble"></i></a></li>
+                                <li><a href="http://instagram.com"><i class="fa fa-instagram"></i></a></li>
+                                <li><a href="http://rssfeed.com"><i class="fa fa-rss"></i></a></li>
+                            </ul>
+                        </div>
+                        <p>Copyright 2015 - 2016. All Rights Reserved. Powered by Themesawesome</p>
+                    </div>
+                </div>
+                <!-- COPYRIGHT END -->
+
+            </footer>
+            <!-- FOOTER END -->
---
+++ HTML/overlandnetwork.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en-US"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en-US"> <![endif]-->
+<!--[if gte IE 8]><html class="ie ie8" lang="en-US"> <![endif]-->
+
+<html dir="ltr" lang="en-US">
+
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+        <!-- Google Fonts
+        ============================================= -->
+        <link href='https://fonts.googleapis.com/css?family=Lato:400,900,700,300' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+
+        <!-- Stylesheets
+        ============================================= -->
+        <link rel="stylesheet" href="css/plugin.css" type="text/css" />
+        <link rel="stylesheet" href="css/style.css" type="text/css" />
+        <link rel="stylesheet" href="css/responsive.css" type="text/css" />
+
+        <!-- JavaScripts
+        ============================================= -->
+        <script type="text/javascript" src="js/jquery.js"></script>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+
+        <!--[if IE]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+
+        <title>Logitrans | Logistics & Transportation HTML Themes</title>
+    </head>
+
+    <body onload="hideTotal()">
+        <div id="preloader">
+            <div id="status">
+                <img src="img/logo.jpg" width="00%" height="auto" alt="Loading..." />
+            </div>
+        </div>
+
+        <!-- MAIN WRAPPER
+        ============================================= -->
+        <div id="main-wrapper" class="clearfix">
+
+            <!-- HEADER
+            ============================================= -->
+            <?php include "header.php"?>
+            <!-- HEADER END -->
+
+            <!-- PAGE TITLE START
+            ============================================= -->
+            <section class="page-title wrapper clearfix" style="background-image: url(img/about-page-bg.jpg);">
+                <div class="container">
+                    <div class="row">
+                        <div class="title-wrap wow fadeIn" data-wow-delay="1s">
+                            <h1>Single Services</h1>
+                            <div class="breadcrumbs">
+                                <p>You Are Here : 
+                                <span><a href="index.html">Home</a></span>
+                                <span class="arrow"><i class="icon icon-arrow-right"></i></span>
+                                <span><a href="services.html">Services</a></span>
+                                <span class="arrow"><i class="icon icon-arrow-right"></i></span>
+                                <span>Single Services</span></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- PAGE TITLE END -->
+
+            <!-- CONTENT START
+            ============================================= -->
+            <section id="content" class="clearfix">
+                <!-- SINGLE SERVICES START
+                ============================================= -->
+                <div class="single-services wrapper bg-color">
+                    <div class="container">
+                        <div class="row">
+                            <div class="single-services-wrap row">
+                                <div class="single-content col-md-9">
+                                    <img src="img/content/service-1.jpg" alt="" />
+                                    <h3 class="title">Overland Network</h3>
+                                    <div class="content-text">
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sedap euydoeiusmod tempor incididunt ut labore et dolore magna aliqua. enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore.</p>
+
+                                        <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt inculpa qui officia deserunt mollit anim id est laborum.</p>
+
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sedap euydoeiusmod tempor incididunt ut labore et dolore magna aliqua. enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.</p>
+                                    </div>
+
+                                    <div class="features-wrap row">
+                                        <div class="col-md-4">
+                                            <div class="features-item wow fadeInUp" data-wow-delay="1.2s">
+                                                <div class="icon-wrap">
+                                                    <i class="icon icon-cargo"></i>
+                                                </div>
+                                                <div class="text">
+                                                    <h4>Sea Delivery</h4>
+                                                    <p>Lorem ipsum dolor sit amet, consec adip tesque tinciunt rutrum sapien, sed diam.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-md-4">
+                                            <div class="features-item wow fadeInUp" data-wow-delay="1.6s">
+                                                <div class="icon-wrap">
+                                                    <i class="icon icon-box-delivery"></i>
+                                                </div>
+                                                <div class="text">
+                                                    <h4>Packaged Goods</h4>
+                                                    <p>Lorem ipsum dolor sit amet, consec adip tesque tinciunt rutrum sapien, sed diam.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-md-4">
+                                            <div class="features-item wow fadeInUp" data-wow-delay="1.9s">
+                                                <div class="icon-wrap">
+                                                    <i class="icon icon-storage"></i>
+                                                </div>
+                                                <div class="text">
+                                                    <h4>Warehousing</h4>
+                                                    <p>Lorem ipsum dolor sit amet, consec adip tesque tinciunt rutrum sapien, sed diam.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="sidebar col-md-3">
+                                    <div class="widget other-services">
+                                        <h4 class="title">Other Services</h4>
+                                        <ul>
+                                            <li><a href="single-services.html">Overland Network</a></li>
+                                            <li><a href="single-services.html">Ocean Netwok</a></li>
+                                            <li><a href="single-services.html">Air Freight</a></li>
+                                            <li><a href="single-services.html">Cargo</a></li>
+                                            <li><a href="single-services.html">Storage</a></li>
+                                            <li><a href="single-services.html">Warehousing</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="widget pdf-download">
+                                        <h4 class="title">PDF Download</h4>
+                                        <ul>
+                                            <li><a href="#"><span><i class="fa fa-file-pdf-o"></i></span>Annual Brochure 2015</a></li>
+                                            <li><a href="#"><span><i class="fa fa-file-pdf-o"></i></span>Annual Brochure 2015 / EN</a></li>
+                                            <li><a href="#"><span><i class="fa fa-file-pdf-o"></i></span>Annual Brochure Services</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="contact-us-banner text-center wow fadeIn" data-wow-delay="0.5s">
+                                <h1>Are you ready to work with us?</h1>
+                                <a href="contact.html" class="button-normal">Contact Us Now</a>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <!-- SINGLE SERVICES END -->
+            </section>
+            <!-- CONTENT END -->
+            
+            <!-- FOOTER START
+            ============================================= -->
+<?php include "footer.php"?>
+            <!-- FOOTER END -->
+
+        </div>
+        <!-- MAIN WRAPPER END -->
+
+        <!-- Footer Scripts
+        ============================================= -->
+        <!-- External -->
+        <script type="text/javascript" src="js/plugin.js"></script>
+        <script type="text/javascript" src="js/main.js"></script>
+
+    </body>
+</html>
